79b5d5c0d6cd68bccfe9ba286b3fc8ef1086fa05
Ossan Msoili <ossanmsoili@yahoo.com>
2023-11-14 16:50:49 +0100

    adding my main

---
+++ my_hunter.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_HUNTER
+** File description:
+** it's my main bro
+*/
+#include <SFML/Graphics.h>
+#include <SFML/System.h>
+#include <SFML/Audio.h>
+#include <SFML/Window.h>
+#include <unistd.h>
+
+int main(void)
+{
+    create_window();
+}
