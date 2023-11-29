edd7d0ab212b9256059ef2cfa697aea04fc49664
Ossan Msoili <ossanmsoili@yahoo.com>
2023-11-29 10:30:40 +0100

    on enlève les printf

--- my_display_in_game.c
+++ my_display_in_game.c
@@ -16,13 +16,9 @@ void my_display_in_game(sfRenderWindow *window, sprite *duck,
     sprite *background_game, sprite *crosshair)
 {
     playing_animation(duck);
-    printf("duck\n");
     display_sprite(window, background_game);
-    printf("background\n");
     display_sprite(window, duck);
-    printf("duck2\n");
     display_sprite(window, crosshair);
-    printf("crosshair\n");
 }
 
 int verif_music_game(music *music, int i, int size)
@@ -38,9 +34,14 @@ int verif_music_game(music *music, int i, int size)
     return i;
 }
 
-void my_display_score(sfRenderWindow *window, sprite *score)
+void my_display_score(sfRenderWindow *window, sprite *score, sprite *duck)
 {
+    sfVector2f mpos = sfSprite_getPosition(duck->sprite);
+
     display_sprite(window, score);
     sfSprite_setScale(score->sprite, (sfVector2f){1.0, 0.9});
     tot_size_sprite(score, 518, 206);
+    if (mpos.x > 1920){
+        playing_tab_animation(score);
+    }
 }
