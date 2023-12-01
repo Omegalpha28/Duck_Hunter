a98ff5ecfddd9f115563350c506903b067429ac7
Ossan Msoili <ossanmsoili@yahoo.com>
2023-12-01 19:37:24 +0100

    ...

--- help.c
+++ help.c
@@ -9,11 +9,9 @@
 
 void my_help(void)
 {
-    my_putstr("USAGE\n     ./my_sokoban map\nDESCRIPTION\n     map  file ");
-    my_putstr("representing the warehouse map, containing ‘#’ for walls,\n");
-    my_putstr("          ");
-    my_putstr("‘P’ for the player, ‘X’ for boxes and ‘O’ for storage ");
-    my_putstr("locations.\n");
+    my_putstr("USAGE\n     ./my_hunter map\nDESCRIPTION\n     we have to kill ");
+    my_putstr("ducks unfortunately the developer had problems during ");
+    my_putstr("the development of the game.\n");
 }
 
 int is_help(char **av)
